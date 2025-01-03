50f4d19dcb8a87718b81f52a9491fe78939b8cf8
Chron1um635 <chron1um522@gmail.com>
2025-01-03 15:34:57 +0300

    make storage manager

--- OverwatchHeroesInfo/Services/StorageManager.swift
+++ OverwatchHeroesInfo/Services/StorageManager.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+import RealmSwift
+
+final class StorageManager {
+    
+    static let shared = StorageManager()
+    
+    let realm: Realm
+    
+    private init() {
+        do {
+            realm = try Realm()
+        } catch {
+            fatalError("Failed to initialize Realm: \(error)")
+        }
+    }
+    
+    func save(hero: Hero) {
+        write {
+            realm.add(hero)
+        }
+    }
+    
+    func delete(hero: Hero) {
+        write {
+            if let heroForDelete = realm.objects(Hero.self).first(where: { heroDB in
+                hero.key == heroDB.key
+            }) {
+                realm.delete(heroForDelete)
+            }
+        }
+    }
+    
+    func receiveHeroStatus(for hero: Hero) -> Bool {
+        realm.objects(Hero.self).contains(hero)
+    }
+    
+    private func write(completion: () -> Void) {
+        do {
+            try realm.write {
+                completion()
+            }
+        } catch {
+            print(error)
+        }
+    }
+}
