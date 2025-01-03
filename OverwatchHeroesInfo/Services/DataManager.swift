1a747a91d10fcc1ac1b8eeb8f603b221c9a35a09
Chron1um635 <chron1um522@gmail.com>
2025-01-03 15:34:43 +0300

    make data manager

--- OverwatchHeroesInfo/Services/DataManager.swift
+++ OverwatchHeroesInfo/Services/DataManager.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+final class DataManager {
+    
+    static let shared = DataManager()
+    
+    private let userDefaults = UserDefaults()
+    
+    private init() {}
+    
+    func setFavoriteStatus(for heroKey: String, with status: Bool) {
+        userDefaults.set(status, forKey: heroKey)
+    }
+    
+    func getFavoriteStatus(for heroKey: String) -> Bool {
+        userDefaults.bool(forKey: heroKey)
+    }
+}
