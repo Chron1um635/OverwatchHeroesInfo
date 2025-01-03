d5b015d183261c6b23a855e99225d1e6416ec1fd
Chron1um635 <chron1um522@gmail.com>
2025-01-03 15:34:31 +0300

    Make network manager

--- OverwatchHeroesInfo/Services/NetworkManager.swift
+++ OverwatchHeroesInfo/Services/NetworkManager.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+import Alamofire
+
+final class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    func fetchData(completion: @escaping (Result<[HeroDataTableObject], AFError>) -> Void) {
+        AF.request("https://overfast-api.tekrop.fr/heroes")
+            .validate()
+            .responseDecodable(of: [HeroDataTableObject].self) { dataResponse in
+                switch dataResponse.result {
+                case .success(let heroes):
+                    completion(.success(heroes))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+        }
+    
+    func fetchDetailsData(heroKey: String, completion: @escaping (Result<HeroDescription, AFError>) -> Void) {
+        AF.request("https://overfast-api.tekrop.fr/heroes/\(heroKey)")
+            .validate()
+            .responseDecodable(of: HeroDescription.self) { dataResponse in
+                switch dataResponse.result {
+                case .success(let heroDescription):
+                    completion(.success(heroDescription))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+    }
+    
+    func fetchImageData(with url: String, completion: @escaping(Data) -> Void) {
+        AF.request(url)
+            .validate()
+            .responseData { dataResponse in
+                switch dataResponse.result {
+                case .success(let data):
+                    completion(data)
+                case .failure(let error):
+                    print(error)
+                }
+            }
+    }
+}
