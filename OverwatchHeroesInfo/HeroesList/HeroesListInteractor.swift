5cca7d5e2596c298f8bbea17802db86462408786
Chron1um635 <chron1um522@gmail.com>
2025-01-03 15:31:34 +0300

    Make HeroListInteractor

--- OverwatchHeroesInfo/HeroesList/HeroesListInteractor.swift
+++ OverwatchHeroesInfo/HeroesList/HeroesListInteractor.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+protocol HeroesListInteractorInputProtocol {
+    init(presenter: HeroesListInteractorOutputProtocol)
+    func fetchHeroes()
+}
+
+protocol HeroesListInteractorOutputProtocol: AnyObject {
+    func heroesDidReceive(with dataStore: HeroesListDataStore)
+}
+
+final class HeroesListInteractor: HeroesListInteractorInputProtocol {
+    
+    private unowned let presenter: HeroesListInteractorOutputProtocol
+    
+    init(presenter: any HeroesListInteractorOutputProtocol) {
+        self.presenter = presenter
+    }
+    
+    func fetchHeroes() {
+        NetworkManager.shared.fetchData { [unowned self] result in
+            switch result {
+            case .success(let data):
+                let dataStore = HeroesListDataStore(heroes: data)
+                presenter.heroesDidReceive(with: dataStore)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    
+}
