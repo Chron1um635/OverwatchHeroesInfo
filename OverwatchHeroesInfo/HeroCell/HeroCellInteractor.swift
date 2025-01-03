5c34fddd14489b1a9b77fe2ea519c5047284d3df
Chron1um635 <chron1um522@gmail.com>
2025-01-03 15:33:37 +0300

    Make HeroCellInteractor

--- OverwatchHeroesInfo/HeroCell/HeroCellInteractor.swift
+++ OverwatchHeroesInfo/HeroCell/HeroCellInteractor.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+protocol HeroCellInteractorInputProtocol {
+    init(presenter: HeroCellInteractorOutputProtocol)
+    func fetchHeroImage(with url: String)
+}
+
+protocol HeroCellInteractorOutputProtocol: AnyObject {
+    func heroImageDidReceive(with data: Data)
+}
+
+final class HeroCellInteractor: HeroCellInteractorInputProtocol {
+    
+    private unowned let presenter: HeroCellInteractorOutputProtocol
+    
+    init(presenter: any HeroCellInteractorOutputProtocol) {
+        self.presenter = presenter
+    }
+    
+    func fetchHeroImage(with url: String) {
+        NetworkManager.shared.fetchImageData(with: url) { [unowned self] data in
+            presenter.heroImageDidReceive(with: data)
+        }
+    }
+    
+}
