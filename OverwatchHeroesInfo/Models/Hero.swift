846483f5f32ff13b86f9d669e2b3766da4559580
Chron1um635 <chron1um522@gmail.com>
2025-01-03 15:34:14 +0300

    Make models

--- OverwatchHeroesInfo/Models/Hero.swift
+++ OverwatchHeroesInfo/Models/Hero.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+import RealmSwift
+
+final class Hero: Object {
+    @Persisted var key: String = ""
+    @Persisted var name: String = ""
+    @Persisted var portrait: String = ""
+    @Persisted var liked: Bool? = false
+}
+
+final class HeroDataTableObject: Decodable {
+    var key: String = ""
+    var name: String = ""
+    var portrait: String = ""
+    var liked: Bool? = false
+}
+
+struct HeroDescription: Decodable {
+    let name: String
+    let description: String
+    let portrait: String
+    let role: String
+    let location: String
+    let age: Int
+    let birthday: String
+    
+    var heroDescription: String? {
+        """
+        Description: \(description)
+        
+        Role: \(role)
+        
+        Location: \(location)
+        
+        Age: \(age)
+        
+        Birthday: \(birthday)
+        """
+    }
+    var liked: Bool? = false
+}
