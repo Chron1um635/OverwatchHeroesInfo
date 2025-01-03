8a5818e7b592c959f3e8332a1e7971262597750e
Chron1um635 <chron1um522@gmail.com>
2025-01-03 15:33:54 +0300

    Make HeroCellConfigurator

--- OverwatchHeroesInfo/HeroCell/HeroCellConfigurator.swift
+++ OverwatchHeroesInfo/HeroCell/HeroCellConfigurator.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+protocol HeroCellConfiguratorInputProtocol {
+    func configure(with view: HeroCell, and presenter: HeroCellPresenterInputProtocol)
+}
+
+final class HeroCellConfigurator: HeroCellConfiguratorInputProtocol {
+    func configure(with view: HeroCell, and presenter: HeroCellPresenterInputProtocol) {
+        let presenter = HeroCellPresenter(view: view, presenter: presenter)
+        let interactor = HeroCellInteractor(presenter: presenter)
+        
+        view.presenter = presenter
+        presenter.interactor = interactor
+    }
+}
