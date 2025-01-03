f966867cd22a79d3ac736adedcbc32119bf01bea
Chron1um635 <chron1um522@gmail.com>
2025-01-03 15:32:22 +0300

    Make HeroListConfigurator

--- OverwatchHeroesInfo/HeroesList/HeroesListConfigurator.swift
+++ OverwatchHeroesInfo/HeroesList/HeroesListConfigurator.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+protocol HeroListConfiguratorInputProtocol {
+    func configure(with view: HeroesListViewController)
+}
+
+final class HeroesListConfigurator: HeroListConfiguratorInputProtocol {
+    func configure(with view: HeroesListViewController) {
+        let presenter = HeroesListPresenter(view: view)
+        let interactor = HeroesListInteractor(presenter: presenter)
+        let router = HeroesListRouter(view: view)
+        
+        view.presenter = presenter
+        presenter.interactor = interactor
+        presenter.router = router
+    }
+}
